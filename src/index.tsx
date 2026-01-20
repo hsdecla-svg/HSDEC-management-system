82a14b800a2284e25075994ffcd9bd6162b492b2
hsdecla-svg <hsdec.la@gmail.com>
2026-01-20 14:26:14 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // ここで App.tsx を探しています
+import App from './App'; // ここを必ず「./App」にする（拡張子.tsxは不要）
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
