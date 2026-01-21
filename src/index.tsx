92408427424bccdcd8dd92bdaf4e8a56b7e665a0
hsdecla-svg <hsdec.la@gmail.com>
2026-01-21 14:25:12 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // 必ず小文字の './app' にしてください
+import App from './MainApp'; // 必ず小文字の './app' にしてください
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(<App />);
+
 
